7ee32618097be5cf2633e2cc148a5817157b2405
Ryan Oliveira <jryancoelho79@hotmail.com>
2023-05-23 00:15:41 -0300

    feat: configuring tailwind and external fonts

---
+++ src/pages/_document.tsx
@@ -0,0 +1,21 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="pt_br">
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+				<meta name="color-scheme" content="dark" />
+
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet" />
+
+				<link rel="icon" href="/favicon.ico" />
+      </Head>
+      <body className="bg-gray-900">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
