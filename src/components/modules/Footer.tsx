cee96c9dfe44f832397cbc7906c9689a8fb1c530
Ryan Oliveira <jryancoelho79@hotmail.com>
2023-05-24 01:08:16 -0300

    feat: creating footer and navbar

---
+++ src/components/modules/Footer.tsx
@@ -0,0 +1,50 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from "next/image";
+import Link from "next/link";
+
+import { InstagramLogo, YoutubeLogo } from "@phosphor-icons/react";
+import { Heading } from "../elements/Heading";
+
+export function Footer() {
+  return (
+    <footer>
+      <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
+        <div
+          className="-mb-6 flex justify-center space-x-12"
+          aria-label="Footer"
+        >
+          <div className="flex flex-col items-center gap-2">
+            <Heading>PARAJÁS</Heading>
+            <span>
+            Soluções Sustentáveis
+            </span>
+          </div>
+        </div>
+        <div className="mt-10 flex items-center justify-center space-x-10">
+          <a
+            href="https://www.instagram.com/parajasbrasil"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <InstagramLogo size={24} />
+          </a>
+          <a
+            href="https://www.youtube.com/@Parajas-kt9ow"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <YoutubeLogo size={24} />
+          </a>
+        </div>
+        <p className="mt-10 text-center text-xs leading-5 text-gray-500">
+          &copy; 2023 Parajás - Todos os direitos reservados.
+        </p>
+        <Link href="https://parajas.com.br">
+          <p className="mt-10 text-center text-xs leading-5 text-gray-500">
+            Feito com ♥ por Parajás.
+          </p>
+        </Link>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/modules/Navbar.tsx
@@ -0,0 +1,51 @@
+import { Heading } from "../elements/Heading";
+import { Text } from "../elements/Text";
+import { clsx } from "clsx";
+
+export function Navbar() {
+  return (
+    <nav className="fixed top-0 z-50 flex h-20 w-full items-center justify-between px-20 shadow-md backdrop-blur-[18px] max-md:px-4">
+      <Heading size="xl" className="text-white">
+        Parajás
+      </Heading>
+
+      <div className="flex items-center space-x-10">
+        <ButtonNavbar text="Início" href="#" active />
+        <ButtonNavbar text="Quem somos" href="#sobre" />
+        <ButtonNavbar text="Produtos" href="#produtos" />
+        <ButtonNavbar text="Equipe" href="#contato" />
+        <ButtonNavbar text="Contato" href="#contato" />
+      </div>
+    </nav>
+  );
+}
+
+type ButtonNavbarProps = {
+  text: string;
+  href: string;
+  active?: boolean;
+};
+
+function ButtonNavbar({ text, href, active }: ButtonNavbarProps) {
+  return (
+    <a href={href} className="group relative flex flex-col items-center gap-1">
+      <Text
+        size="lg"
+        className={clsx("transition-colors group-hover:text-gray-100", {
+          "text-gray-500": !active,
+          "text-white": active,
+        })}
+      >
+        {text}
+      </Text>
+      <span
+        className={clsx(
+          "absolute -bottom-1 h-0 w-0 bg-yellow-300 transition-all group-hover:h-[1px] group-hover:w-10/12",
+          {
+            "h-[1px] w-10/12": active,
+          }
+        )}
+      />
+    </a>
+  );
+}
