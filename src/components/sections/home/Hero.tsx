3d816fcf038fe582bc9ebf4db7660765db2afc40
Ryan Oliveira <jryancoelho79@hotmail.com>
2023-05-24 19:57:27 -0300

    feat: implementing learn more button is fixed bg on some devices

--- src/components/sections/home/Hero.tsx
+++ src/components/sections/home/Hero.tsx
@@ -3,14 +3,13 @@ import { Heading } from "~/components/elements/Heading";
 import { Text } from "~/components/elements/Text";
 
 export function Hero() {
-
   const handleGoAbout = () => {
     const about = document.getElementById("sobre");
     about?.scrollIntoView({ behavior: "smooth" });
-  }
+  };
 
   return (
-    <section className="flex h-screen w-full flex-col items-center justify-center gap-8 bg-[url('/static/images/cotijuba-bg.webp')] bg-cover px-20 max-md:gap-4 max-md:px-4">
+    <section className="flex h-screen w-full flex-col items-center justify-center gap-8 bg-[url('/static/images/cotijuba-bg.webp')] bg-cover bg-center px-20 transition-shadow max-md:gap-4 max-md:px-4 ">
       <Heading asChild size="6xl" className="text-white">
         <strong className="text-5xl max-md:text-center max-md:text-2xl">
           UM MUNDO MAIS{" "}
@@ -22,7 +21,10 @@ export function Hero() {
         Vamos juntos nessa jornada?
       </Text>
 
-      <Button className="w-48 max-w-[12rem] bg-yellow-300 text-black-900" onClick={handleGoAbout}>
+      <Button
+        className="w-48 max-w-[12rem] bg-yellow-300 text-black-900"
+        onClick={handleGoAbout}
+      >
         Saiba mais
       </Button>
     </section>
