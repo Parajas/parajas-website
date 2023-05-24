e89b5820c9b570a3b0a81180eb0ef4d0daa829ec
Ryan Oliveira <jryancoelho79@hotmail.com>
2023-05-24 01:59:13 -0300

    feat: tailwind color tweaks

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,24 @@ export default {
       fontFamily: {
 				sans: "Montserrat, sans-serif",
 			},
+
+      colors: {
+        gray: {
+          900: '#121214',
+          800: '#202024',
+          400: '#7c7c8a',
+          200: '#c4c4cc',
+          100: '#e1e1e6',
+        },
+
+        black: {
+          900: "#000",
+          800: "#464646",
+          700: "#A3A3A3",
+          400: "#ECECEC",
+          100: "#F6F6F6",
+        },
+      }
     },
   },
   plugins: [],
