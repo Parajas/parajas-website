8a612f89a90f197933c49228fc6532c1b15286f3
Ryan Oliveira <jryancoelho79@hotmail.com>
2023-05-29 03:38:21 -0300

    fix: fixing react hook forms typing problem

--- src/components/modules/ContactForm.tsx
+++ src/components/modules/ContactForm.tsx
@@ -1,11 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-// This example uses `@web3forms/react` plugin and tailwindcss for css styling
-
-import { useState, useEffect } from "react";
-import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { useForm,  } from "react-hook-form";
 import useWeb3Forms from "@web3forms/react";
 
 import { env } from "../../env.mjs";
@@ -22,7 +19,7 @@ export function ContactForm() {
     formState: { errors, isSubmitSuccessful, isSubmitting },
   } = useForm({
     mode: "onTouched",
-  });
+  }) as any;
   const [isSuccess, setIsSuccess] = useState(false);
   const [message, setMessage] = useState(false);
 
@@ -76,7 +73,6 @@ export function ContactForm() {
           />
           {errors.name && (
             <div className="mt-1 text-red-600">
-              {/* @ts-expect-error - TODO: FIX THIS */}
               <small>{errors.name.message}</small>
             </div>
           )}
@@ -107,7 +103,6 @@ export function ContactForm() {
           />
           {errors.email && (
             <div className="mt-1 text-red-600">
-              {/* @ts-expect-error - TODO: FIX THIS */}
               <small>{errors.email.message}</small>
             </div>
           )}
@@ -127,42 +122,10 @@ export function ContactForm() {
           />
           {errors.message && (
             <div className="mt-1 text-red-600">
-              {/* @ts-expect-error - TODO: FIX THIS */}
-
               <small>{errors.message.message}</small>
             </div>
           )}
         </div>
-
-        {/* <button
-          type="submit"
-          className="dark:text-black w-full rounded-md bg-gray-900 px-7 py-4 font-semibold text-white transition-colors hover:bg-gray-800 focus:outline-none focus:ring focus:ring-gray-200 focus:ring-offset-2 dark:bg-white "
-        >
-          {isSubmitting ? (
-            <svg
-              className="dark:text-black mx-auto h-5 w-5 animate-spin text-white"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <circle
-                className="opacity-25"
-                cx="12"
-                cy="12"
-                r="10"
-                stroke="currentColor"
-                strokeWidth="4"
-              ></circle>
-              <path
-                className="opacity-75"
-                fill="currentColor"
-                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-              ></path>
-            </svg>
-          ) : (
-            "Send Message"
-          )}
-        </button> */}
 
         <Button>
           {isSubmitting ? (
